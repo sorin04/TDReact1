0645edaa58905e819c3d08320dcf4bd2a9d96742
sorin04 <sorin.pirgari@lyceeastier.com>
2025-01-10 19:09:05 +0100

    3

---
+++ src/users/ModeToken.ts
@@ -0,0 +1,8 @@
+export default class ModeToken{
+    token: string | null = null
+    role: string | null = null
+    username: string | null = null
+    id_user: number | null = null
+    TokenTimeValid: number | null = null
+
+}
